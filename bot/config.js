6700bd4136105544f7e80ae0190530b5fc5efe13
tekkay <wellingtontekay@gmail.com>
2017-11-16 22:15:30 -0200

    arquivo final

---
+++ bot/config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+ consumer_key:         'InAmFTW7EXb9812l43taO455y',
+  consumer_secret:      'HWuIvT85E5YZddzdbd8pewTjpgZR9yv4CBW6g7aPn508o9Aedc',
+  access_token:         '3054768551-TkUqh6DJYXO6ag575dJkfoBrqfFw3vHq6cH2Utd',
+  access_token_secret:  'iBdSXcXwMUgvnRxWTfWpmfijhOXYL6mF5gHFgLTNVpTaB',
+}
